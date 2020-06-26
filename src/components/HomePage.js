3f93a796981b3167ed4f32768e7e4eb9073c12d5
Jenny Dinh <jennythi.d@gmail.com>
2020-06-26 17:04:12 -0400

    add landing page and style

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import SignUp from './SignUp';
 
 class HomePage extends Component {
+
     render() {
         return(
         <div>
             <div className="">
                 <button>Login</button>
                 <button>SignUp</button>
+                <div class="heroContainer">
+                    <div className="headerText">
+                        <h1>What's for Dinner?</h1>
+                    </div>
+                    <div class="inner"></div>
+                </div>
                 <div>
                     <SignUp />
                 </div>
