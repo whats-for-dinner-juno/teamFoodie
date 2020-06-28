a39ea4052f7c77c63987477c4293525bda063f2a
Jenny Dinh <jennythi.d@gmail.com>
2020-06-28 12:03:40 -0400

    pre deploy to page

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import Search from './Search';
 import './../styles/App.scss';
@@ -7,17 +6,14 @@ import HomePage from './HomePage';
 import PartyName from './partyGroup/PartyName';
 import firebase from './../firebase';
 
-=======
 import React, { Component } from "react";
 import Search from "./Search";
 import "./../styles/App.scss";
 import "./HomePage";
 import HomePage from "./HomePage";
->>>>>>> e7efea9e53b5d93b96ee13e7802a24355cb57c95
 class App extends Component {
 
   render() {
-<<<<<<< HEAD
   return (
     <div className="App">
       <HomePage />
@@ -26,16 +22,7 @@ class App extends Component {
       {/* <Search /> */}
     </div>
   );
-=======
-    return (
-      <div className="App">
-        <HomePage />
-
-        {/* importing just for test purposes */}
-        <Search />
-      </div>
-    );
->>>>>>> e7efea9e53b5d93b96ee13e7802a24355cb57c95
+ 
   }
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Account from './authenication/Account';
-<<<<<<< HEAD
-=======
 import Referral from './Referral';
->>>>>>> e7efea9e53b5d93b96ee13e7802a24355cb57c95
 
 class HomePage extends Component {
 
