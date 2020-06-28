2578f0f7bf01cefb562c82286121c387f1e632df
Scott Carter <scottandrewcarter1@hotmail.com>
2020-06-28 12:08:01 -0400

    Updates

---
+++ precache-manifest.bc2c01e1b679de3b581875b9e45a0b66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7252bcb6ab9e004bbd19cadd8e7cee",
+    "url": "/teamFoodie/index.html"
+  },
+  {
+    "revision": "0e3a680bd523f433f577",
+    "url": "/teamFoodie/static/css/main.4f44fbaf.chunk.css"
+  },
+  {
+    "revision": "0a243f551053364612ff",
+    "url": "/teamFoodie/static/js/2.cb326f30.chunk.js"
+  },
+  {
+    "revision": "2d70f05d135acc7c7cd2dabe17002c00",
+    "url": "/teamFoodie/static/js/2.cb326f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3a680bd523f433f577",
+    "url": "/teamFoodie/static/js/main.c921120a.chunk.js"
+  },
+  {
+    "revision": "cf7f36565ffad4f5639c",
+    "url": "/teamFoodie/static/js/runtime-main.516d192f.js"
+  },
+  {
+    "revision": "39b05cef843fb793df4a76fd93515df4",
+    "url": "/teamFoodie/static/media/Souffle-Pancake.39b05cef.jpg"
+  },
+  {
+    "revision": "6f35eb7d22db22b0c584ebb723312c1d",
+    "url": "/teamFoodie/static/media/burger.6f35eb7d.jpg"
+  },
+  {
+    "revision": "9cfa4031baf40198166b73beb21d975e",
+    "url": "/teamFoodie/static/media/dessert.9cfa4031.jpg"
+  }
+]);
